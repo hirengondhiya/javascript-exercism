b9df0d834d9fda48c572705cb8e17c23ca7b31e2
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-04-30 09:51:28 +1000

    soln for two-fer

---
+++ two-fer/two-fer.spec.js
@@ -0,0 +1,19 @@
+import { twoFer } from './two-fer'
+
+describe('twoFer()', () => {
+  test('no name given', () => {
+    expect(twoFer()).toEqual("One for you, one for me.")
+  })
+
+  xtest('invalid datatype name given', () => {
+    expect(twoFer(123)).toEqual("One for you, one for me.")
+  })
+
+  test('a name given', () => {
+    expect(twoFer("Alice")).toEqual("One for Alice, one for me.")
+  })
+
+  test('another name given', () => {
+    expect(twoFer("Bob")).toEqual("One for Bob, one for me.")
+  })
+})
