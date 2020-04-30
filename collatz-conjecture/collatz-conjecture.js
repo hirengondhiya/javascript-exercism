c73455dbd11b4c3285dac2e822fba5df807ce613
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-04-30 18:47:09 +1000

    solution for Collatz Conjecture challenge

---
+++ collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+function* collatzConjecture(n) {
+  let newN = n;
+  while(newN > 1) {
+    if(newN % 2 == 0) {
+      newN = newN / 2;
+    } else {
+      newN = 3 * newN + 1;
+    }
+    yield newN;
+  }
+}
+
+export const steps = (n) => {
+  if(n > 0) {
+    return [...collatzConjecture(n)].length
+  }
+  throw new Error("Only positive numbers are allowed");
+};
