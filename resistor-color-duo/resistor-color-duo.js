968200aae8b9f6ff5fb03efe813734ca77a9500c
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-05-11 19:56:22 +1000

    refactored the solution to use arrays and array methods

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,5 +1,8 @@
-import {colorCode} from './resistor-color';
+const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
 
 export const decodedValue = (colors) => {
-  return (colorCode(colors[0]).toString() + colorCode(colors[1]).toString()) * 1;
+  return colors
+            .filter((c, i) => i < 2) // take first two elements only, this line can be removed if we want to add up all the passed color values
+            .reverse() // reverse them
+            .reduce((sum, color, index) => sum + COLORS.indexOf(color) * 10 ** index, 0); // using reducer sum all the values as we do with decimal system
 };
